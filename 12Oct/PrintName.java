bb330c1a47406eef8d67eecd21ea2ec7437f8e44
kajitverma <113059029+kajitverma@users.noreply.github.com>
2022-10-15 23:25:56 +0530

    Add files via upload

---
+++ 12Oct/PrintName.java
@@ -0,0 +1,10 @@
+class PrintName
+{
+	public static void main(String args[])
+	{
+		System.out.println("First Name: "+args[0]+" Last Name:  "+args[1]);
+		System.out.println("Age: "+args[0]);
+		System.out.println("Course: "+args[0]);
+		System.out.println("Score: "+args[0]);
+	}
+}	
---
+++ 12Oct/PrintName1.java
@@ -0,0 +1,8 @@
+class PrintName1
+{
+	public static void main(String args[])
+	{
+		System.out.println("First Name: "+args[0] +" Last Name:  "+args[1] +" Age: "+args[2] + " Course: "+args[3] + "Score: "+args[4]);
+		
+	}
+}	
